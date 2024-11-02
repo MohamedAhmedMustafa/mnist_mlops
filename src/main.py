0143bbef17dddeb34462e491c3a9fc7c4c0f5eb8
MohamedAhmedMustafa <mohamedahmedmustafa22@gmail.com>
2024-11-02 14:53:59 +0200

    Adding cm

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from sklearn.datasets import load_digits
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
+from sklearn.metrics import confusion_matrix
 
 # load data set
 
@@ -17,8 +18,13 @@ model = RandomForestClassifier()
 
 model.fit(X,y)
 
+# test model
+
 results = model.predict(X)
 accuracy = accuracy_score(y,results)
 print("Accuracy= ",accuracy)
 precision = precision_score(y,results, average='macro')
 print("precision= ",precision)
+cm = confusion_matrix(y,results)
+print("Confusiion matrix:")
+print(cm)
